128e5c1edfc4c37fc4120be72bce6d25d1c29d5b
Felix <felix.lapierre1@gmail.com>
2020-03-15 11:16:44 -0400

    Challenge 3 working but too slow

--- challenge3.js
+++ challenge3.js
@@ -5,6 +5,15 @@ var buffer = "";
 var stage = 0;
 
 var index = {};
+var success = 0;
+
+function indexEncoding(base64) {
+    return base64.substring(50, 60)
+    + base64.substring(100, 110)
+    + base64.substring(150, 160)
+    + base64.substring(500, 510)
+    + base64.substring(1000, 1010);
+}
 
 module.exports = function(data, clientWrite) {
     //console.log(data)
@@ -17,9 +26,10 @@ module.exports = function(data, clientWrite) {
 
     if(stage == 1) {
         buffer += data.toString();
-        if(data.indexOf("\n\n") != -1) {
+        const length = data.length;
+        if(data.charAt(length - 1) == "\n" && data.charAt(length - 2) == "\n") {
             if(buffer.charAt(0) == "T" || buffer.charAt(0) == "W") {
-                console.log(buffer.toString());
+                console.log(buffer.toString() + " Succeeded " + success + " times.");
                 return;
             }
             if(buffer.charAt(0) == 'F') {
@@ -28,9 +38,16 @@ module.exports = function(data, clientWrite) {
                 console.log("I did it!!");
                 return;
             }
-            var json = JSON.parse(buffer);
+
+            try {
+                var json = JSON.parse(buffer);
+            } catch(e) {
+                console.log("Timed out after " + success + " tries");
+                process.exit(1);
+            }
             buffer = "";
-            console.log("Received message");
+            //console.log("Received message "+ success);
+            success++;
             const response = {};
             response.collection_id = json.collection_id;
             response.allowed_products = [];
@@ -38,18 +55,14 @@ module.exports = function(data, clientWrite) {
             json.collection_products.forEach((product) => {
                 var id = product.product_id;
                 var base64 = product.product_image;
-                var indexed64 = base64.substring(50, 80);
+                var indexed64 = indexEncoding(base64);
                 if(index[indexed64]) {
                     if(index[indexed64].allowed) {
                         response.allowed_products.push(id)
-                        console.log("allowing " + indexed64);
-                    } else {
-                        console.log("disallowing " + indexed64);
                     }
-                    writeBase64File(base64.substring(50, 110), base64)
                 } else {
                     console.log("Cache miss on " + indexed64)
-                    index[indexed64] = {allowed: false};
+                    index[indexed64] = {allowed: true};
                     writeBase64File(indexed64, base64)
                     response.allowed_products.push(id);
                 }
@@ -60,14 +73,14 @@ module.exports = function(data, clientWrite) {
             }
             const message = `{ "collection_id": "${response.collection_id}", "allowed_products": [${products}] }\n\n`;
             clientWrite(message);
-            console.log(message);
+            //console.log(message);
             fs.writeFileSync("index.json", JSON.stringify(index));
         }
     }
 }
 
 function writeBase64File(id, base64) {
-    var filename = "images/" + id.replace('/', '') + ".html";
+    var filename = "images/" + id.replace(/\//g, '') + ".html";
 
     const fileData = `<!DOCTYPE html>
     <html>
@@ -79,4 +92,14 @@ function writeBase64File(id, base64) {
       </body>
     </html>`
     fs.writeFileSync(filename, fileData);
+}
+
+function printImagesThatFailed() {
+    previous.forEach((entry) => {
+        var id = "No";
+        if(entry.allowed) {
+            id = "Yes"
+        }
+        writeBase64File(`${entry.counter} ${id} ${indexEncoding(entry.base64)}`, entry.base64);
+    })
 }
